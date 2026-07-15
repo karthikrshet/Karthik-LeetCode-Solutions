c359f9bd3bcb2230f39054348b09c992d62ac6c7
Karthik Rajesh Shet <kartikrshet@gmail.com>
2026-07-15 19:57:45 +0530

    Time: 1 ms (83.11%), Space: 42.2 MB (96.78%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        // The GCD of the sum of the first n odd numbers (n^2) 
+        // and the first n even numbers (n^2 + n) is always just n.
+        return n;
+    }
+}
