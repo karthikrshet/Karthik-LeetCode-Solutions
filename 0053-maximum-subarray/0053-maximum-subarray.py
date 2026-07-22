1ae51a54877dda3f82eb4aec31fb170821033ebe
Karthik Rajesh Shet <kartikrshet@gmail.com>
2026-07-23 00:54:46 +0530

    Time: 34 ms (63.92%), Space: 31.4 MB (43.31%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = max_sum = nums[0]
+        
+        for num in nums[1:]:
+            # Either extend the previous subarray or start a new one from the current element
+            current_sum = max(num, current_sum + num)
+            # Update the global maximum sum found so far
+            max_sum = max(max_sum, current_sum)
+            
+        return max_sum
