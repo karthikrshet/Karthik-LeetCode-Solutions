33823ad64522a116712419ac523f1021adcdade6
Karthik Rajesh Shet <kartikrshet@gmail.com>
2026-07-22 23:15:51 +0530

    Time: 0 ms (100%), Space: 20 MB (42.57%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        # If target is not found, 'left' indicates the correct insertion index
+        return left
