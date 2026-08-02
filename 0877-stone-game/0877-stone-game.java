e4fbb7edca05f42a1286dd1f1b4a3f1be18cb2d2
Karthik Rajesh Shet <kartikrshet@gmail.com>
2026-08-02 13:44:36 +0530

    Time: 0 ms (100%), Space: 43.1 MB (62.03%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        // Alice always wins because she can control the game strategy 
+        // to always pick the optimal set of piles due to even length.
+        return true;
+    }
+}
